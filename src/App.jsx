39ef61af41fa8201e444fc96cf7699ca17e68145
Retno Wati <retnowati3099@gmail.com>
2023-04-18 12:39:53 +0700

    menambahkan show dan unshow upload foto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { useState } from "react";
 
 function App() {
+  const [showUploadImg, setShowUploadImg] = useState(false);
+
   return (
     <div className="container">
       <div className="row">
@@ -14,6 +17,9 @@ function App() {
               cursor: "pointer",
             }}
             className="position-relative"
+            onClick={() => {
+              setShowUploadImg(!showUploadImg);
+            }}
           >
             <img
               src="src/assets/IMG_20180426_160424_490.jpg"
@@ -22,12 +28,17 @@ function App() {
             />
             <div
               className="position-absolute top-0 start-0 w-100 h-100 rounded-circle"
-              style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+              style={{ backgroundColor: "rgba(64, 64, 64, 0.8)" }}
             ></div>
             <div className="position-absolute top-0 end-0 bottom-0 start-0 d-flex flex-column align-items-center justify-content-center text-light">
               <i className="bi bi-camera-fill fs-3 "></i>
               <p className="w-50 text-center">ADD PROFILE PHOTO</p>
             </div>
+            {showUploadImg && (
+              <div className="position-absolute top-50 start-50 bg-secondary rounded p-3 bg-white w-100 shadow-sm">
+                Upload Photo
+              </div>
+            )}
           </div>
         </div>
       </div>
