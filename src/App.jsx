65d493efc33ee15f6edc65d2662487c6f66b8577
Retno Wati <retnowati3099@gmail.com>
2023-04-18 17:23:36 +0700

    update dikit pada show upload photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
-  const [showUploadImg, setShowUploadImg] = useState(false);
+  const [showUploadImg, setShowUploadImg] = useState({
+    isVisible: false,
+    position: {
+      x: 0,
+      y: 0,
+    },
+  });
+  const [photo, setPhoto] = useState(null);
+
+  const handleAreaClick = (e) => {
+    setShowUploadImg({
+      isVisible: !showUploadImg.isVisible,
+      position: {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    });
+  };
+
+  const fileInputRef = useRef(null);
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    //console.log("Selected file:", file);
+    setPhoto(file);
+    console.log(photo);
+  };
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col-6">
+        <div className="col mx-auto">
           <div
             style={{
               width: "200px",
@@ -17,9 +46,7 @@ function App() {
               cursor: "pointer",
             }}
             className="position-relative"
-            onClick={() => {
-              setShowUploadImg(!showUploadImg);
-            }}
+            onClick={handleAreaClick}
           >
             <img
               src="src/assets/IMG_20180426_160424_490.jpg"
@@ -32,12 +59,29 @@ function App() {
             ></div>
             <div className="position-absolute top-0 end-0 bottom-0 start-0 d-flex flex-column align-items-center justify-content-center text-light">
               <i className="bi bi-camera-fill fs-3 "></i>
-              <p className="w-50 text-center">ADD PROFILE PHOTO</p>
+              <p className="w-50 text-center user-select-none">
+                ADD PROFILE PHOTO
+              </p>
             </div>
-            {showUploadImg && (
-              <div className="position-absolute top-50 start-50 bg-secondary rounded p-3 bg-white w-100 shadow-sm">
-                Upload Photo
-              </div>
+            {showUploadImg.isVisible && (
+              <>
+                <div
+                  className="position-absolute rounded p-3 bg-white w-100 shadow-sm user-select-none"
+                  style={{
+                    left: showUploadImg.position.x - 80,
+                    top: showUploadImg.position.y + 10,
+                  }}
+                  onClick={handleClick}
+                >
+                  Upload Photo
+                </div>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleFileInputChange}
+                />
+              </>
             )}
           </div>
         </div>
